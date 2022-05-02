ec735e8ed2d5ca82db0400a372f44811d1275020
rwdled <79921530+rwdled@users.noreply.github.com>
2022-05-02 10:22:48 -0500

    actually works lol

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("armed")
+})
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 490; index++) {
+        music.playMelody("C5 B C5 B C5 B C5 B ", 1000)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
+basic.showString("unarmed")
 basic.forever(function () {
+	
+})
+control.inBackground(function () {
 	
 })
