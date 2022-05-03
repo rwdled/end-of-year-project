d88ff556ca553969b40727f20a66ce20dffc4439
rwdled <79921530+rwdled@users.noreply.github.com>
2022-05-03 13:31:28 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onSound(DetectedSound.Loud, function () {
     for (let index = 0; index < 490; index++) {
-        music.playMelody("C5 B C5 B C5 B C5 B ", 1000)
+        music.playMelody("C5 B C5 B C5 B C5 B ", 2000)
     }
 })
 input.onButtonPressed(Button.AB, function () {
